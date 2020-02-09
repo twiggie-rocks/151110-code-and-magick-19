ddec3f3ea1d88bac6e860d989f8bd3c438d4b7d1
twiggie-rocks <arbor.parva@gmail.com>
2020-02-09 14:07:37 +0700

    Оптимизировала создание массива с волшебниками

--- js/setup.js
+++ js/setup.js
@@ -21,29 +21,17 @@ var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кр
 var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var wizardsNumber = 4;
+var wizards = [];
 
-var wizards = [
-  {
+for (var j = 0; j < wizardsNumber; j++) {
+  var newWizard = {
     name: generateRandom(firstNames) + ' ' + generateRandom(lastNames),
     coatColor: generateRandom(coatColors),
     eyeColor: generateRandom(eyesColors)
-  },
-  {
-    name: generateRandom(firstNames) + ' ' + generateRandom(lastNames),
-    coatColor: generateRandom(coatColors),
-    eyeColor: generateRandom(eyesColors)
-  },
-  {
-    name: generateRandom(firstNames) + ' ' + generateRandom(lastNames),
-    coatColor: generateRandom(coatColors),
-    eyeColor: generateRandom(eyesColors)
-  },
-  {
-    name: generateRandom(firstNames) + ' ' + generateRandom(lastNames),
-    coatColor: generateRandom(coatColors),
-    eyeColor: generateRandom(eyesColors)
-  }
-];
+  };
+  wizards.push(newWizard);
+}
 
 // создать DOM-элементы для волшебников
 
