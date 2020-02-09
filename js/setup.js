46118079a20577bfdfc218af660e4dda242686b3
twiggie-rocks <arbor.parva@gmail.com>
2020-02-09 20:53:51 +0700

    Добавила возможность изменять персонажа

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,60 @@
 'use strict';
 
-// показать блок .setup
+// открытие и закрытие блока .setup
 
-document.querySelector('.setup').classList.remove('hidden');
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 
-// сгенерировать случайные данные
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var setupName = setup.querySelector('input[name=username]');
+
+var popupEscHandler = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', popupEscHandler);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', popupEscHandler);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+
+setupName.addEventListener('focus', function () {
+  document.removeEventListener('keydown', popupEscHandler);
+});
+
+setupName.addEventListener('blur', function () {
+  document.addEventListener('keydown', popupEscHandler);
+});
+
+// функция генерации случайных данных
 
 var generateRandom = function (arr) {
   var min = 0;
@@ -15,12 +65,48 @@ var generateRandom = function (arr) {
   return arr[random];
 };
 
-// создать массив, описывающий персонажей
+// данные для отрисовки персонажей
 
 var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastNames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+// смена цвета мантии, глаз и фаербола по нажатию
+
+var wizardCoat = setup.querySelector('.wizard-coat');
+var wizardEyes = setup.querySelector('.wizard-eyes');
+var wizardFireball = setup.querySelector('.setup-fireball-wrap');
+
+var wizardCoatInput = setup.querySelector('input[name=coat-color]');
+var wizardEyesInput = setup.querySelector('input[name=eyes-color]');
+var wizardFireballInput = setup.querySelector('input[name=fireball-color]');
+
+var changeFill = function (element, color, input) {
+  element.style.fill = color;
+  input.value = color;
+};
+
+var changeBackground = function (element, color, input) {
+  element.style.background = color;
+  input.value = color;
+};
+
+wizardCoat.addEventListener('click', function () {
+  changeFill(wizardCoat, generateRandom(coatColors), wizardCoatInput);
+});
+
+wizardEyes.addEventListener('click', function () {
+  changeFill(wizardEyes, generateRandom(eyesColors), wizardEyesInput);
+});
+
+wizardFireball.addEventListener('click', function () {
+  changeBackground(wizardFireball, generateRandom(fireballColors), wizardFireballInput);
+});
+
+// создать массив, описывающий персонажей
+
 var wizardsNumber = 4;
 var wizards = [];
 
